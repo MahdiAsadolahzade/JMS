1ee7aea733e6ff7c6dbbbb9c5d5d9bc571f5dce6
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2023-11-01 14:40:14 +0330

    Update section of languages

--- src/Components/Language/LanguageSelect.tsx
+++ src/Components/Language/LanguageSelect.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { useAppStore } from '../../appStore';
-import { FaGlobe, FaChevronDown } from 'react-icons/fa'; 
+import React from "react";
+import { useAppStore } from "../../appStore";
+import { FaGlobe, FaChevronDown } from "react-icons/fa";
+import ir from "../../../public/Country/ir.svg";
+import us from "../../../public/Country/us.svg";
 
 const LanguageSelect: React.FC = () => {
-  const { language, setLanguage } = useAppStore();
+  const { language, setLanguage, darkMode } = useAppStore();
 
   const handleLanguageChange = (selectedLanguage: string) => {
     setLanguage(selectedLanguage);
@@ -13,17 +15,33 @@ const LanguageSelect: React.FC = () => {
     <div className="relative inline-block">
       <div className="group">
         <button
-          className="flex items-center cursor-pointer bg-gray-800 text-white py-2 px-4 rounded"
-          onClick={() => setLanguage(language === 'English' ? 'Farsi' : 'English')}
+          className="flex items-center cursor-pointer  text-white py-2 px-3 rounded"
+          onClick={() =>
+            setLanguage(language === "English" ? "Farsi" : "English")
+          }
         >
-          <FaGlobe className="mr-1" />
+          <div
+            className="w-[25px] h-[25px] m-1 rounded-full"
+            style={{
+              backgroundImage: `url(${language === "English" ? us : ir})`,
+              backgroundSize: "cover",
+            }}
+          ></div>
           {language}
           <FaChevronDown className="ml-1" />
         </button>
-        <div className="hidden group-hover:block absolute top-full left-0 w-24 py-2 bg-gray-800 text-white border border-gray-600 rounded z-10">
+        <div
+          className={`hidden group-hover:block absolute top-full left-0 w-24 py-2 ${
+            darkMode
+              ? "bg-gray-800 border-gray-600"
+              : "bg-blue-500 border-blue-300"
+          } text-white border  rounded z-10`}
+        >
           <div
-            className="flex items-center px-2 py-1 hover:bg-gray-600 cursor-pointer"
-            onClick={() => handleLanguageChange('English')}
+            className={`flex items-center px-2 py-1 ${
+              darkMode ? "hover:bg-gray-600" : "hover:bg-blue-400"
+            }  cursor-pointer`}
+            onClick={() => handleLanguageChange("English")}
           >
             <span role="img" aria-label="USA Flag" className="mr-1">
               🇺🇸
@@ -31,8 +49,10 @@ const LanguageSelect: React.FC = () => {
             English
           </div>
           <div
-            className="flex items-center px-2 py-1 hover:bg-gray-600 cursor-pointer"
-            onClick={() => handleLanguageChange('Farsi')}
+            className={`flex items-center px-2 py-1 ${
+              darkMode ? "hover:bg-gray-600" : "hover:bg-blue-400"
+            } cursor-pointer`}
+            onClick={() => handleLanguageChange("Farsi")}
           >
             <span role="img" aria-label="Iran Flag" className="mr-1">
               🇮🇷
